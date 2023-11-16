d4797b96890cf889ce825b07cebc9e8e1b753566
Nikus666 <94123146+Nikus66@users.noreply.github.com>
2023-11-16 16:58:38 +0100

    lab3
    nadrabiam zaleglosci z domu wiec musialem sie pobawic z plikami

---
+++ Lab1/Constructions/Construction.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1.Constructions.Models;
+
+namespace Lab1.Constructions
+{
+    internal class Construction
+    {
+        public Construction(float height, float width, int entrances, int humanCapacity, TypeOfMaterial buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+
+        public Construction(CreateConstructionDto constructionDto)
+        {
+            Height = constructionDto.Height;
+            Width = constructionDto.Width;
+            Entrances = constructionDto.Entrances;
+            HumanCapacity = constructionDto.HumanCapacity;
+            BuildMaterial = constructionDto.BuildMaterial;
+        }
+
+        public Construction()
+        {
+            Height = 100;
+            Width = 100;
+            Entrances = 3;
+            HumanCapacity = 20;
+            BuildMaterial = TypeOfMaterial.Concrete;
+        }
+
+        private TypeOfMaterial _buildMaterial { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public TypeOfMaterial BuildMaterial
+        {
+            get
+            {
+                return _buildMaterial;
+            }
+            set
+            {
+                _buildMaterial = value;
+            }
+        }
+
+        public double GetSquareCost()
+        {
+            if (BuildMaterial == TypeOfMaterial.Brick)
+            {
+                return Width * Height * 0.9 * 0.8;
+            }
+            else if (BuildMaterial == TypeOfMaterial.Concrete)
+            {
+                return Width * Height * 0.9 * 0.87;
+            }
+            else if (BuildMaterial == TypeOfMaterial.Wood)
+            {
+                return Width * Height * 0.9 * 0.78;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Lab1.Constructions;
+using Lab1.Constructions.Models;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = new Construction(32, 23, 3, 6, TypeOfMaterial.Brick);
+            Console.WriteLine(constr.BuildMaterial);
+
+            var constr2 = new Construction();
+            Console.WriteLine(constr2.Width);
+
+            var constructionDto = new CreateConstructionDto {
+                Height = 21,
+                Width = 37,
+                Entrances = 3,
+                HumanCapacity = 6,
+                BuildMaterial = TypeOfMaterial.Wood
+            };
+
+            var constr3 = new Construction(constructionDto);
+            Console.WriteLine(constr3.BuildMaterial);
+
+            Console.WriteLine(constr3.GetSquareCost());
+
+        }
+    }
+}
